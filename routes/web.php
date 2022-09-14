3b405f053e5b6325698c357cabb6554ca86f22c5
ariwiraa <ariwiraatmaja@gmail.com>
2022-09-14 14:23:20 +0700

    add config midtrans

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,14 @@ Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('det
 Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
 Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::post('/cart/{id}', [FrontendController::class, 'cartAdd'])->name('cart-add');
+    Route::delete('/cart/{id}', [FrontendController::class, 'cartDelete'])->name('cart-delete');
+    Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+    Route::post('/checkout', [FrontendController::class, 'checkout'])->name('checkout');
+    Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 
